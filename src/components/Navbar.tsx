9eb6f107275ccb1e2b572fcbf97063e049b8f2ce
leonard800 <leonardabimanyu@gmail.com>
2025-06-21 16:59:30 +0700

    fix: css styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,21 @@
-"use client";
-import { Link } from "react-router-dom";
-
-export default function Navbar() {
+export default function Navbar () {
   return (
-    <nav className="flex items-center justify-between h-20 w-full px-8 bg-white shadow">
+    <nav className="flex items-center justify-between w-full px-8 py-4 bg-white shadow">
       <div className="flex items-center">
         <img src="/logo.png" alt="Alba Logo" className="h-10 w-auto" />
       </div>
 
       <div className="flex items-center space-x-8">
-        <Link to="/" className="text-black font-semibold hover:text-yellow-400">About</Link>
-        <Link to="/" className="text-black font-semibold hover:text-yellow-400">Service</Link>
-        <Link to="/" className="text-black font-semibold hover:text-yellow-400">Portfolio</Link>
-        <Link to="/" className="text-black font-semibold hover:text-yellow-400">Career</Link>
-        <Link
-          to="/login"
+        <a href="#about" className="text-black font-semibold hover:text-yellow-400">About</a>
+        <a href="#service" className="text-black font-semibold hover:text-yellow-400">Service</a>
+        <a href="#portfolio" className="text-black font-semibold hover:text-yellow-400">Portfolio</a>
+        <a href="#career" className="text-black font-semibold hover:text-yellow-400">Career</a>
+        <a
+          href="/login"
           className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-6 rounded-full transition duration-300"
         >
           Login
-        </Link>
+        </a>
       </div>
     </nav>
   );
